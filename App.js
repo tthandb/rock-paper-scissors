ee683e5f2436732d7b1ca74d374de37c22a402cf
tthandb <tthandb@hotmail.com>
2020-07-15 04:15:29 +0700

    init

--- App.js
+++ App.js
@@ -1,6 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const CHOICES = [
+  {
+    name: "rock",
+    uri: "http://pngimg.com/uploads/stone/stone_PNG13622.png",
+  },
+  {
+    name: "paper",
+    uri: "https://www.stickpng.com/assets/images/5887c26cbc2fc2ef3a186046.png",
+  },
+  {
+    name: "scissors",
+    uri:
+      "http://pluspng.com/img-png/png-hairdressing-scissors-beauty-salon-scissors-clipart-4704.png",
+  },
+];
 
 export default function App() {
   return (
@@ -14,8 +30,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
