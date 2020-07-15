a42f92da7342406c6fa20bbca949db739bd1cde8
tthandb <tthandb@hotmail.com>
2020-07-15 17:25:16 +0700

    done milestone 3

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 
 const CHOICES = [
   {
@@ -17,21 +23,102 @@ const CHOICES = [
       "http://pluspng.com/img-png/png-hairdressing-scissors-beauty-salon-scissors-clipart-4704.png",
   },
 ];
+const Button = (props) => (
+  <TouchableOpacity
+    style={styles.buttonStyle}
+    onPress={() => props.onPress(props.name)}
+  >
+    <Text style={styles.buttonText}>
+      {props.name.charAt(0).toUpperCase() + props.name.slice(1)}
+    </Text>
+  </TouchableOpacity>
+);
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gamePrompt: "Choose your weapon!",
+    };
+  }
+  onPress = (userChoice) => {
+    console.log("userChoice", userChoice);
+  };
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text>{this.state.gamePrompt}</Text>
+        {CHOICES.map((choice) => {
+          return (
+            <Button
+              key={choice.name}
+              name={choice.name}
+              onPress={this.onPress}
+            />
+          );
+        })}
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#e9ebee",
+  },
+  buttonContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonStyle: {
+    width: 200,
+    margin: 10,
+    height: 50,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#640D14",
+  },
+  buttonText: {
+    fontSize: 25,
+    color: "white",
+    fontWeight: "bold",
+  },
+  choicesContainer: {
+    margin: 10,
+    borderWidth: 2,
+    paddingTop: 100,
+    shadowRadius: 5,
+    paddingBottom: 100,
+    borderColor: "grey",
+    shadowOpacity: 0.9,
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "white",
+    justifyContent: "space-around",
+    shadowColor: "rgba(0,0,0,0.2)",
+    shadowOffset: { height: 5, width: 5 },
+  },
+  choiceContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+  choiceDescription: {
+    fontSize: 25,
+    color: "#250902",
+    fontWeight: "bold",
+    textDecorationLine: "underline",
+  },
+  choiceCardTitle: {
+    fontSize: 30,
+    color: "#250902",
+  },
+  choiceImage: {
+    width: 150,
+    height: 150,
+    padding: 10,
   },
 });
